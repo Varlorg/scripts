ff3b21c1e1687150a52267ce423d45b4edc122e7
Varlorg <varlorg@gmail.com>
2024-04-03 09:29:40 +0000

    Add script template

---
+++ template_cmd.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+###################
+# Variables       #
+###################
+
+readonly SCRIPT_FULLPATH="$(realpath "${BASH_SOURCE[-1]}")"
+readonly SCRIPT_DIR="$(dirname "$SCRIPT_FULLPATH")"
+
+# Public functions pattern
+# all expect the ones starting by '_'
+# pattern: ^<funtion_name>(){
+readonly FUNCTION_PATTERN='^[^_][[:graph:]]*()\s*{'
+
+######################
+# Internal Functions #
+######################
+_internal_fct() {
+    echo "Call only form inside the script"
+}
+
+####################
+# Public Functions #
+####################
+
+# default default_action
+default_action() {
+    echo "Default action when no argument given"
+}
+
+###################
+# Generic script  #
+###################
+
+# Help command
+usage(){
+    echo "Usage: $0 <command>"
+    echo "Commands availables:"
+    # Display the functions matching the pattern
+    # Print the line before as description
+    grep -B1 "${FUNCTION_PATTERN}" "$0" | \
+        sed -e 's/()\s*{//' \
+        -e 's/^#\s*//' \
+        -e '/--/d' | \
+        awk '{a=$0; getline; print $0";"a}' | \
+        column -t -s ';'
+}
+
+# Script is being run directly
+if [ "${BASH_SOURCE[0]}" = "${0}" ]; then
+  if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then usage; exit 0; fi
+  if [ -z "$1" ]; then #Default action when no argument given 
+    default_action
+  else
+    # Allow command only the function matching the pattern
+    INTERNAL_FUNCTIONS=$(grep "${FUNCTION_PATTERN}" "$0" | \
+      sed -e 's/()\s*{//' -e '/--/d')
+
+    echo "${INTERNAL_FUNCTIONS}" | grep -e "^$1$" > /dev/null ||
+      { echo "$1: Bad command" ; usage; exit 1;}
+
+    $@
+  fi
+fi
