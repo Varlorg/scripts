40732fa5d5da023d024fc0c70448f115d49427bc
Varlorg <varlorg@gmail.com>
2026-02-16 21:37:19 +0100

    run shfmt and shellchek on template_cmd script

--- template_cmd.sh
+++ template_cmd.sh
@@ -3,8 +3,10 @@
 # Variables       #
 ###################
 
-readonly SCRIPT_FULLPATH="$(realpath "${BASH_SOURCE[-1]}")"
-readonly SCRIPT_DIR="$(dirname "$SCRIPT_FULLPATH")"
+SCRIPT_FULLPATH="$(realpath "${BASH_SOURCE[-1]}")"
+readonly SCRIPT_FULLPATH
+SCRIPT_DIR="$(dirname "$SCRIPT_FULLPATH")"
+readonly SCRIPT_DIR
 
 # Public functions pattern
 # all expect the ones starting by '_'
@@ -15,7 +17,7 @@ readonly FUNCTION_PATTERN='^[^_][[:graph:]]*()\s*{'
 # Internal Functions #
 ######################
 _internal_fct() {
-    echo "Call only form inside the script"
+	echo "Call only form inside the script"
 }
 
 ####################
@@ -24,7 +26,7 @@ _internal_fct() {
 
 # default default_action
 default_action() {
-    echo "Default action when no argument given"
+	echo "Default action when no argument given"
 }
 
 ###################
@@ -32,32 +34,40 @@ default_action() {
 ###################
 
 # Help command
-usage(){
-    echo "Usage: $0 <command>"
-    echo "Commands availables:"
-    # Display the functions matching the pattern
-    # Print the line before as description
-    grep -B1 "${FUNCTION_PATTERN}" "$0" | \
-        sed -e 's/()\s*{//' \
-        -e 's/^#\s*//' \
-        -e '/--/d' | \
-        awk '{a=$0; getline; print $0";"a}' | \
-        column -t -s ';'
+usage() {
+	echo "Usage: $0 <command>"
+	echo "Commands availables:"
+	# Display the functions matching the pattern
+	# Print the line before as description
+	grep -B1 "${FUNCTION_PATTERN}" "$0" |
+		sed -e 's/()\s*{//' \
+			-e 's/^#\s*//' \
+			-e '/--/d' |
+		awk '{a=$0; getline; print $0";"a}' |
+		column -t -s ';'
 }
 
 # Script is being run directly
 if [ "${BASH_SOURCE[0]}" = "${0}" ]; then
-  if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then usage; exit 0; fi
-  if [ -z "$1" ]; then #Default action when no argument given 
-    default_action
-  else
-    # Allow command only the function matching the pattern
-    INTERNAL_FUNCTIONS=$(grep "${FUNCTION_PATTERN}" "$0" | \
-      sed -e 's/()\s*{//' -e '/--/d')
+	if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+		usage
+		exit 0
+	fi
+	if [ -z "$1" ]; then #Default action when no argument given
+		default_action
+	else
+		# Allow command only the function matching the pattern
+		INTERNAL_FUNCTIONS=$(grep "${FUNCTION_PATTERN}" "$0" |
+			sed -e 's/()\s*{//' -e '/--/d')
 
-    echo "${INTERNAL_FUNCTIONS}" | grep -e "^$1$" > /dev/null ||
-      { echo "$1: Bad command" ; usage; exit 1;}
+		echo "${INTERNAL_FUNCTIONS}" | grep -e "^$1$" >/dev/null ||
+			{
+				echo "$1: Bad command"
+				usage
+				exit 1
+			}
 
-    $@
-  fi
+		# shellcheck disable=SC2068 # splitting elements is wanted
+		$@
+	fi
 fi
